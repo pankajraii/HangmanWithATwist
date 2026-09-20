6b443d2ed635c4a026579ea142fa62b37c2e9162
Pankaj Rai <pankajthulung006@gmail.com>
2026-09-20 14:07:59 +0530

    Program.cs

--- Program.cs
+++ Program.cs
@@ -5,9 +5,7 @@ namespace HangmanTwist
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+       
         [STAThread]
         static void Main()
         {
